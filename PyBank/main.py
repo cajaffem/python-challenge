7980c22f44cdcb94f0d0ae681ba0c18e29f07921
cajaffem <christian.a.jaffe@gmail.com>
2020-01-03 00:16:23 -0500

    "latest version"

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,12 +4,9 @@ import csv
 filename = os.path.join("..", "Data_files", "pybank_data.csv") 
 
 # Lists to store data
-title = []
-price = []
-subscribers = []
-reviews = []
-review_percent = []
-length = []
+date = []
+profit_losses = []
+monthly_change_pl = []
 
 with open(filename, newline="") as csvfile:
     # CSV reader specifies delimiter and variable that holds contents
@@ -17,10 +14,24 @@ with open(filename, newline="") as csvfile:
 
     print(csvreader)
 
-    # Read the header row first (skip this step if there is no header)
+    #Read the header row first
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
 
     # Read each row of data after the header
     for row in csvreader:
-        print(sum(row))
+        date.append(row[0])
+        profit_losses.append(row[1])
+# convert profit_losses from string to integer
+    pl_numeric = list(map(int,profit_losses))
+    for i in range (1, len(profit_losses)):
+       monthly_change_pl.append(pl_numeric[i]-pl_numeric[i-1])
+    average_change = sum(monthly_change_pl)/len(monthly_change_pl)
+
+    print ("Financial Analysis")
+#create dimension to store total months
+    months_total = len(date)
+    print(f"Total Months: {months_total}")
+    sumof_pl = sum(pl_numeric)
+    print(f"Total: ${sumof_pl}")
+    print(f"Average Change: ${round(average_change, 2)}")
