836e1e974af240ae8e4c24dc44b900f9102e7a59
cajaffem <christian.a.jaffe@gmail.com>
2020-01-06 20:56:28 -0500

    "added final pypoll code minus txt file export"

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,7 +1,7 @@
 import os
 import csv
 
-filename = os.path.join("..", "Data_files", "pypoll_test_data.csv")
+filename = os.path.join("..", "Data_files", "election_data.csv")
 
 # initialize lists to store data
 voter_id = []
@@ -14,17 +14,14 @@ with open(filename, newline="") as csvfile:
 
     print(csvreader)
 
+# Read out header
     csv_header = next(csvreader)
-    print (csv_header)
 
+# Read each row of data after the header
     for row in csvreader:
         voter_id.append(row[0])
         county.append(row[1])
         candidate.append(row[2])
-
-    print (voter_id [0], len(voter_id))
-    print (county [0])
-    print (candidate [0])
 
 # create new list for unique candidates
     indv_cand = []
@@ -33,11 +30,10 @@ with open(filename, newline="") as csvfile:
     for x in candidate:
         if x not in indv_cand:
                 indv_cand.append(x)
-    print(indv_cand)
+    #print(indv_cand) returns list of all candidates
 
 # create variable for total votes cast
     tot_votes = len(voter_id)
-    print(tot_votes)
 
 # determine total number of votes per candidate
     votes_Khan = candidate.count("Khan")
@@ -51,7 +47,22 @@ with open(filename, newline="") as csvfile:
     percent_Li = votes_Li/len(voter_id) * 100
     percent_OTooley = votes_OTooley/len(voter_id) * 100
 
-# write conditional using the votes_ variable to find candidate with most votes
-# for mostvotes in candidate
-# if votes_khan > votes_correy and votes_li and votes_otooley then
-# print mostvotes
+# print final poll report + conditional to determine overall winner as final print statement
+    print("Election Results")
+    print("-------------------------")
+    print(f"Total Votes: {tot_votes}")
+    print("-------------------------")
+    print(f"Khan: {round(percent_Khan, 2)}% ({votes_Khan})")
+    print(f"Correy: {round(percent_Correy, 2)}% ({votes_Correy})")
+    print(f"Li: {round(percent_Li, 2)}% ({votes_Li})")
+    print(f"O'Tooley: {round(percent_OTooley, 2)}% ({votes_OTooley})")
+    print("-------------------------")
+    if votes_Khan > votes_Correy and votes_Li and votes_OTooley:
+        print("Winner: Khan")
+    elif votes_Correy > votes_Khan and votes_Li and votes_OTooley:
+        print ("Winner: Correy")
+    elif votes_Li > votes_Khan and votes_Correy and votes_OTooley:
+        print ("Winner: Li")
+    elif votes_OTooley > votes_Khan and votes_Correy and votes_Li:
+        print ("Winner: O'Tooley")
+    print("-------------------------")
